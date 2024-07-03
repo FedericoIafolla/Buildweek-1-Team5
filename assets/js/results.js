9cb8de0530fd29a7a36b9b3644df76e0c171fc77
John-9813 <john.oliver98.br@gmail.com>
2024-07-03 19:39:00 +0200

    Co-authored-by: FedericoIafolla <FedericoIafolla@users.noreply.github.com> Co-authored-by: Romina-Timis <Romina-Timis@users.noreply.github.com> Co-authored-by: Nicolomecca <Nicolomecca@users.noreply.github.com> Co-authored-by: mary1457 <mary1457@users.noreply.github.com> Co-authored-by: Davide Malavenda <d-mala@users.noreply.github.com>

--- assets/js/results.js
+++ assets/js/results.js
@@ -12,109 +12,118 @@
 //     localStorage.removeItem('quizResults'); // Rimuove i risultati dopo averli mostrati
 // });
 const doughnutLabel = {
-    id: 'doughnutLabel',
-    beforeDraw: function (chart, args, pluginOptions) {
-        const { ctx, data } = chart;
-        ctx.save();
-        const centerX = chart.width / 2;
-        const centerY = chart.height / 2 - 50;
+  id: "doughnutLabel",
+  beforeDraw: function (chart, args, pluginOptions) {
+    const { ctx, data } = chart;
+    ctx.save();
+    const centerX = chart.width / 2;
+    const centerY = chart.height / 2 - 50;
+    
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
 
-        const lines = pluginOptions.text.split('\n');
-        const fontSize = pluginOptions.fontSize || 12;
-        const lineHeight = pluginOptions.lineHeight || 1.5;
-        const boldFont = `${fontSize * 1.6}px sans-serif`;
-        const regularFont = `${fontSize * 1.3}px sans-serif`;
+    const lines = pluginOptions.text.split("\n");
+    const fontSize = pluginOptions.fontSize || 12;
+    const lineHeight = pluginOptions.lineHeight || 1.5;
+    const boldFont = `${fontSize * 1.6}px sans-serif`;
+    const regularFont = `${fontSize * 1}px sans-serif`;
 
-        ctx.textAlign = 'center';
+    ctx.textAlign = "center";
 
-        lines.forEach((line, index) => {
-            if (index < 2) {
-                ctx.font = boldFont;
-                if (index === 1) {
-                    ctx.fillStyle = pluginOptions.colorSecondLine;
-                } else {
-                    ctx.fillStyle = pluginOptions.color;
-                }
-            } else {
-                ctx.font = regularFont;
-                ctx.fillStyle = pluginOptions.color;
-            }
+    lines.forEach((line, index) => {
+      if (index < 2) {
+        ctx.font = boldFont;
+        if (index === 1) {
+          ctx.fillStyle = pluginOptions.colorSecondLine;
+        } else {
+          ctx.fillStyle = pluginOptions.color;
+        }
+      } else {
+        ctx.font = regularFont;
+        ctx.fillStyle = pluginOptions.color;
+      }
 
-            ctx.fillText(line, centerX, centerY + (index * fontSize * lineHeight));
-        });
+      ctx.fillText(line, centerX, centerY + index * fontSize * lineHeight);
+    });
 
-        ctx.restore();
-    }
+    ctx.restore();
+  },
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-    const quizResults = JSON.parse(localStorage.getItem('quizResults'));
+document.addEventListener("DOMContentLoaded", function () {
+  const quizResults = JSON.parse(localStorage.getItem("quizResults"));
 
-    if (quizResults) {
-        const correctAnswers = document.getElementById('correct-answers');
-        const wrongAnswers = document.getElementById('wrong-answers');
-        const correctAnswersPercentage = document.getElementById('correct-answers-percentage');
-        const wrongAnswersPercentage = document.getElementById('wrong-answers-percentage');
+  if (quizResults) {
+    const correctAnswers = document.getElementById("correct-answers");
+    const wrongAnswers = document.getElementById("wrong-answers");
+    const correctAnswersPercentage = document.getElementById(
+      "correct-answers-percentage"
+    );
+    const wrongAnswersPercentage = document.getElementById(
+      "wrong-answers-percentage"
+    );
 
-        correctAnswers.innerHTML = `${quizResults.correct} / ${quizResults.total} questions`;
-        wrongAnswers.innerHTML = `${quizResults.wrong} / ${quizResults.total} questions`;
+    correctAnswers.innerHTML = `${quizResults.correct} / ${quizResults.total} questions`;
+    wrongAnswers.innerHTML = `${quizResults.wrong} / ${quizResults.total} questions`;
 
-        const correctAnswersPercentageNumber = (quizResults.correct / parseInt(quizResults.total)) * 100;
-        const wrongAnswersPercentageNumber = (quizResults.wrong / parseInt(quizResults.total)) * 100;
+    const correctAnswersPercentageNumber =
+      (quizResults.correct / parseInt(quizResults.total)) * 100;
+    const wrongAnswersPercentageNumber =
+      (quizResults.wrong / parseInt(quizResults.total)) * 100;
 
-        correctAnswersPercentage.innerHTML = `${correctAnswersPercentageNumber}%`;
-        wrongAnswersPercentage.innerHTML = `${wrongAnswersPercentageNumber}%`;
+    correctAnswersPercentage.innerHTML = `${correctAnswersPercentageNumber}%`;
+    wrongAnswersPercentage.innerHTML = `${wrongAnswersPercentageNumber}%`;
 
-        const ctx = document.getElementById('myChart').getContext('2d');
+    const ctx = document.getElementById("myChart").getContext("2d");
 
-        let text, colorSecondLine;
-        if (correctAnswersPercentageNumber >= 60) {
-            text = 'Congratulations!\nYou passed the exam.\n\nWe\'ll send you the certificate \nin few minutes. Check your email \n(including promotions/spam folder)';
-            colorSecondLine = '#01FBFC';
-        } else {
-            text = 'We are sorry\nYou failed the exam\n\nYou will be contacted by \nyour teacher in the next few days';
-            colorSecondLine = '#C2128D';
-        }
-
-        new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: ['Sbagliate', 'Corrette'],
-                datasets: [{
-                    data: [quizResults.wrong, quizResults.correct],
-                    backgroundColor: [
-                        '#C2128D',
-                        '#00FFFF'
-                    ],
-                    borderColor: [
-                        '#C2128D',
-                        '#00FFFF'
-                    ],
-                    borderWidth: 1,
-                }]
-            },
-            options: {
-                responsive: false,
-                maintainAspectRatio: false,
-                cutout: '70%',
-                plugins: {
-                    legend: {
-                        display: false
-                    },
-                    title: {
-                        display: false
-                    },
-                    doughnutLabel: {
-                        fontSize: 13,
-                        lineHeight: 1.5,
-                        color: 'white',
-                        colorSecondLine: colorSecondLine,
-                        text: text
-                    }
-                }
-            },
-            plugins: [doughnutLabel]
-        });
+    let text, colorSecondLine;
+    if (correctAnswersPercentageNumber >= 60) {
+      text =
+        "Congratulations!\nYou passed the exam.\n\nWe'll send you the certificate \nin few minutes. Check your email \n(including promotions/spam folder)";
+      colorSecondLine = "#01FBFC";
+    } else {
+      text =
+        "We are sorry\nYou failed the exam\n\nYou will be contacted by \nyour teacher in the next few days";
+      colorSecondLine = "#C2128D";
     }
-    localStorage.removeItem('quizResults');
+
+    new Chart(ctx, {
+      type: "doughnut",
+      data: {
+        labels: ["Sbagliate", "Corrette"],
+        datasets: [
+          {
+            data: [quizResults.wrong, quizResults.correct],
+            backgroundColor: ["#C2128D", "#00FFFF"],
+            borderColor: ["#C2128D", "#00FFFF"],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false,
+        cutout: "70%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: false,
+          },
+          doughnutLabel: {
+            fontSize: 13,
+            lineHeight: 1.5,
+            color: "white",
+            colorSecondLine: colorSecondLine,
+            text: text,
+          },
+        },
+      },
+      plugins: [doughnutLabel],
+    });
+  }
+  localStorage.removeItem("quizResults");
 });
