59c264277590d2219380927985312377594497da
John-9813 <john.oliver98.br@gmail.com>
2024-07-05 11:48:05 +0200

    Co-authored-by: Romina-Timis <Romina-Timis@users.noreply.github.com> Co-authored-by: FedericoIafolla <FedericoIafolla@users.noreply.github.com>

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -2,69 +2,65 @@ const text = document.getElementById("myText");
 const stars = document.querySelectorAll("span");
 let indicecliccato = 0;
 let click = false;
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 
+// Funzione che colora le stelle al nostro click
 function init() {
-    stars.forEach((element, i) => {
-        element.addEventListener("click", function () {
-            if (i===0 &&  click===false){
-                click=true;
-                colorstar(i); 
-                indicecliccato=i;
-            }
-            else if (i===0 && click) {
-                stars[0].style.color = "white";
-                indicecliccato=0;
-                click=false;
-            }
-            else {colorstar(i); 
-                indicecliccato=i;}
-
-        })
-        // Abbiamo utilizzato un Event Listener per il mouseover su una stella
-        // dove all'interno richiama la funzione per rimuovere la selezione da tutte le stella
-        // e quella per selezionare la stella su cui si è posato il mouse
-        //abbiamo lavorato in modo simile per il mouseout nel codice sottostante
-        element.addEventListener("mouseover", function () {
-            deselect();
-            colorstar(i);
-
-
-
-        })
-        element.addEventListener("mouseout", function () { 
-            deselect();
-            if (indicecliccato>0 || click) {
-                colorstar(indicecliccato);
-            }
-        })
-
+  stars.forEach((element, i) => {
+    element.addEventListener("click", function () {
+      if (i === 0 && click === false) {
+        click = true;
+        colorstar(i);
+        indicecliccato = i;
+      } else if (i === 0 && click) {
+        stars[0].style.color = "white";
+        indicecliccato = 0;
+        click = false;
+      } else {
+        colorstar(i);
+        indicecliccato = i;
+      }
     });
 
-};
+    // Abbiamo utilizzato un Event Listener per il mouseover su una stella
+    // dove all'interno richiama la funzione per rimuovere la selezione da tutte le stella
+    // e quella per selezionare la stella su cui si è posato il mouse
+    element.addEventListener("mouseover", function () {
+      deselect();
+      colorstar(i);
+    });
+
+    element.addEventListener("mouseout", function () {
+      deselect();
+      if (indicecliccato > 0 || click) {
+        colorstar(indicecliccato);
+      }
+    });
+  });
+}
+
 // Utilizziamo questa funzione per colorare le stelle attraverso un ciclo
 function colorstar(i) {
-    for (let y = 0; y <= i; y++) {
-        stars[y].style.color = "#00FFFF";
-    }
+  for (let y = 0; y <= i; y++) {
+    stars[y].style.color = "#00FFFF";
+  }
 }
-
-
 
 function deselect() {
-    stars.forEach((element, i) => {
-        element.style.color = "white";
-    })
-
+  stars.forEach((element, i) => {
+    element.style.color = "white";
+  });
 }
+
 // Event listener per il taso enter nella textarea
 text.addEventListener("keydown", function (event) {
-    //se il tasto enter viene premuti
-    if (event.key === "Enter") {
-        //mostra un messaggio di conferma e resetta il valore della textarea
-        alert("We have received your feedback!");
-        text.value = "";
-    }
-})
-link2.addEventListener("click", function(){
-    text.value = "";})
+  //se il tasto enter viene premuto
+  if (event.key === "Enter") {
+    //mostra un messaggio di conferma e resetta il valore della text area
+    alert("We have received your feedback!");
+    text.value = "";
+  }
+});
+link2.addEventListener("click", function () {
+  text.value = "";
+});
--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,29 +1,26 @@
 const check = document.getElementById("myCheckbox");
 const btnProceed = document.getElementById("btnProceed");
-const link =document.getElementById("link");
+const link = document.getElementById("link");
 const vuoto = document.getElementById("vuoto");
 const pieno = document.getElementById("pieno");
-window.addEventListener('load', init);
+window.addEventListener("load", init);
 
-function init() {
-    
-};
+function init() {}
 
-link.addEventListener("click", function(){
-    check.checked= false;
+// "Ascolta" il click sul checkbox
+link.addEventListener("click", function () {
+  check.checked = false;
 });
 
+// Funzione che abilita il pulsante "Proceed" dopo il checkbox
 check.addEventListener("click", function () {
-    if (check.checked) {
-        btnProceed.disabled = false;
-        pieno.style.display= "initial";
-        vuoto.style.display= "none";
-    }
-    else {
-        btnProceed.disabled = true; 
-        pieno.style.display= "none";
-        vuoto.style.display= "initial";
-    }
-    }
-
-);
+  if (check.checked) {
+    btnProceed.disabled = false;
+    pieno.style.display = "initial";
+    vuoto.style.display = "none";
+  } else {
+    btnProceed.disabled = true;
+    pieno.style.display = "none";
+    vuoto.style.display = "initial";
+  }
+});
